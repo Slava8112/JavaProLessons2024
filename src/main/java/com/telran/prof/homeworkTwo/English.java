2067aaf446f742cdee24684bfe217e776cb7facc
slava <sv8997608@gmail.com>
2024-02-20 22:15:52 +0100

    homeworkTwo

---
+++ src/main/java/com/telran/prof/homeworkTwo/English.java
@@ -0,0 +1,13 @@
+package com.telran.prof.homeworkTwo;
+
+import java.util.Arrays;
+
+public class English extends Alphabet{
+    String[] englishAlphabet = new String []{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+
+    @Override
+    public void printAlphabet(){
+        super.printAlphabet();
+        System.out.println(Arrays.toString(englishAlphabet));
+    }
+}
---
+++ src/main/java/com/telran/prof/homeworkTwo/Main.java
@@ -0,0 +1,19 @@
+package com.telran.prof.homeworkTwo;
+
+public class Main {
+    public static void main(String[] args) {
+        English english = new English();
+        english.setName("English alphabet");
+        english.printAlphabet();
+
+        Russian russian =new Russian();
+        russian.setName("Russian alphabet");
+        russian.printAlphabet();
+
+        German german = new German();
+        german.setName("German alphabet");
+        german.printAlphabet();
+
+//        c)в классе основной программы добавить метод printAllInfo(), который в качестве параметра должен принимать массив алфавитов и выводить их в консоль
+    }
+}
